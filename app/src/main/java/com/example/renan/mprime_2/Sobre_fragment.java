5b58d31bd283c72da0377e446c25dd4b9205c17c
Renan Cunha <renan.cunha33@gmail.com>
2015-10-11 17:09:25 -0300

    Sobre (HTML + bootstrap)

--- app/src/main/java/com/example/renan/mprime_2/Sobre_fragment.java
+++ app/src/main/java/com/example/renan/mprime_2/Sobre_fragment.java
@@ -6,19 +6,28 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 /**
  * Created by Renan on 30/09/2015.
  */
 public class Sobre_fragment extends Fragment {
-
+    WebView webView;
     View MyView;
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         MyView = inflater.inflate(R.layout.fragment_sobre, container, (false));
         ((MainActivity) getActivity())
                 .setActionBarTitle("Sobre");
+        webView = (WebView) MyView.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setSaveFormData(true);
+        webView.loadUrl("file:///android_res/raw/index.html");
         return MyView;
     }
 
