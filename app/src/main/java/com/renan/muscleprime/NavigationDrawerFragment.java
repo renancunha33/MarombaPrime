647fbaf30956fb469aa697380e8b173cdb6132f2
Renan Cunha <renan.cunha33@gmail.com>
2015-12-12 20:11:32 -0200

    correção de bug.

--- app/src/main/java/com/renan/muscleprime/NavigationDrawerFragment.java
+++ app/src/main/java/com/renan/muscleprime/NavigationDrawerFragment.java
@@ -265,7 +265,7 @@ public class NavigationDrawerFragment extends Fragment {
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayShowTitleEnabled(true);
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setTitle("Muscle Prime");
+        actionBar.setTitle(R.string.app_name);
     }
 
     private ActionBar getActionBar() {
